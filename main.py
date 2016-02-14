826980898c69d3d5353b4b26307ee845a54ef9ee
Fed <fedffm@gmail.com>
2016-02-13 23:36:40 -0700

    Implemented accuracy

--- main.py
+++ main.py
@@ -7,17 +7,25 @@ dataset = 0
 
 if dataset == 0:
     filename = "datasets/iris.data"
-    targets = ['setosa', 'virginica', 'versicolor']
+    targets = ['Iris-setosa', 'Iris-virginica', 'Iris-versicolor']
 elif dataset == 1:
     filename = "datasets/pima-indians-diabetes.data"
     targets = [0, 1]
 
-""" Create the network """
+# Create the network
 nn = NeuralNetwork()
 nn.loadDataset(filename)
 nn.normalize()
 nn.createNetwork([2, 3], targets)
+numCorrect = 0
 
+# Make the predictions
 for i in range(len(nn.testingSet)):
     nn.feed(nn.testingSet[i])
-    print("Instance", i + 1, "is predicted to be:", nn.getClassification())
+    print("Instance", i + 1, ": predicted =", nn.getClassification(), "actual =", nn.testingSet[i][-1])
+    if nn.getClassification() == nn.testingSet[i][-1]:
+        numCorrect += 1
+
+# Output the accuracy
+accuracy = float(numCorrect) / float(len(nn.testingSet)) * 100
+print("Accuracy:", float("{0:.1f}".format(accuracy)), '%')
